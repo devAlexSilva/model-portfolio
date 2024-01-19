f81a3d2f40c9f30bed891b95ffa8f9b21e6ee82b
Alex Kitsu <9apsilva@gmail.com>
2024-01-19 12:09:57 -0300

    feat: added 3d animate stars

--- components/HeroContent.tsx
+++ components/HeroContent.tsx
@@ -10,7 +10,7 @@ const HeroContent = () => {
     <motion.div
       initial='hidden'
       animate='visible'
-      className='flex flex-row items-center justify-center px-20 mt-24 w-full z-20'
+      className='flex flex-row items-center justify-center px-20 mt-40 w-full z-20'
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center mt-auto text-start">
         <motion.div
